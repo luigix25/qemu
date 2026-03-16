14e5574eb3b4986dc2bd4a9dd7aba9dc177b548f
Luigi Leonardi <leonardi@redhat.com>
2026-03-16 17:08:24 +0100

    hw/i386: Add device tree generation for x64
    Generate a flattened device tree describing devices that belong to non-guest
    planes (e.g. SVSM firmware plane). The FDT includes virtio-mmio devices whose
    plane property differs from the machine's device_plane, allowing firmware to
    discover its assigned devices.
    
    The implementation is based on hw/i386/microvm-dt.c.
    
    Signed-off-by: Luigi Leonardi <leonardi@redhat.com>

---
+++ hw/i386/x86_dt_common.c
@@ -0,0 +1,128 @@
+/*
+ * x86 device tree generation for device plane filtering
+ *
+ * Generate a flattened device tree describing devices that belong to
+ * non-guest planes (e.g. SVSM firmware plane).  The resulting FDT is
+ * passed to the firmware via IGVM so it can discover virtio-mmio
+ * assigned to its plane.
+ *
+ * The device tree is intentionally incomplete: CPUs and memory are
+ * not included as firmware obtains that information through other
+ * channels.
+ *
+ * Based on hw/i386/microvm-dt.c
+ *
+ * Copyright (c) Red Hat, Inc.
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+#include "qemu/osdep.h"
+#include "qemu/cutils.h"
+#include "qemu/error-report.h"
+#include "qapi/error.h"
+#include "system/device_tree.h"
+#include "hw/core/sysbus.h"
+#include "hw/virtio/virtio-mmio.h"
+#include "qom/object.h"
+
+#include "x86_dt_common.h"
+#include <libfdt.h>
+
+
+static bool debug;
+
+static void dt_add_virtio(MachineState *ms, VirtIOMMIOProxy *mmio)
+{
+    SysBusDevice *dev = SYS_BUS_DEVICE(mmio);
+    VirtioBusState *mmio_virtio_bus = &mmio->bus;
+    BusState *mmio_bus = &mmio_virtio_bus->parent_obj;
+    VirtIODevice *vdev;
+    char *nodename;
+
+    if (QTAILQ_EMPTY(&mmio_bus->children)) {
+        return;
+    }
+
+    /* Get the plane property of the inner device */
+    vdev = virtio_bus_get_device(mmio_virtio_bus);
+    uint8_t plane = object_property_get_int(OBJECT(vdev), "plane",
+                                            &error_fatal);
+
+    /* Device is intended for guest usage */
+    if (plane == ms->device_plane) {
+        return;
+    }
+
+    hwaddr base = dev->mmio[0].addr;
+    hwaddr size = 512;
+
+    nodename = g_strdup_printf("/virtio_mmio@%" PRIx64, base);
+    qemu_fdt_add_subnode(ms->fdt, nodename);
+    qemu_fdt_setprop_string(ms->fdt, nodename, "compatible", "virtio,mmio");
+
+    qemu_fdt_setprop_cell(ms->fdt, nodename, "plane", plane);
+
+    qemu_fdt_setprop_sized_cells(ms->fdt, nodename, "reg", 2, base, 2, size);
+    qemu_fdt_setprop(ms->fdt, nodename, "dma-coherent", NULL, 0);
+    g_free(nodename);
+}
+
+static void dt_setup_sys_bus(MachineState *ms)
+{
+    BusState *bus;
+    BusChild *kid;
+    Object *obj;
+
+    bus = sysbus_get_default();
+
+    QTAILQ_FOREACH(kid, &bus->children, sibling) {
+        DeviceState *dev = kid->child;
+
+        /* virtio */
+        obj = object_dynamic_cast(OBJECT(dev), TYPE_VIRTIO_MMIO);
+        if (obj) {
+            dt_add_virtio(ms, VIRTIO_MMIO(obj));
+            continue;
+        }
+
+        if (debug) {
+            fprintf(stderr, "%s: unhandled: %s type: %s\n", __func__,
+                    dev->canonical_path,
+                    object_get_typename(OBJECT(dev)));
+        }
+    }
+}
+
+int dt_setup_x86(MachineState *ms)
+{
+    int fdt_size = 0;
+
+    g_free(ms->fdt);
+    ms->fdt = create_device_tree(&fdt_size);
+
+    /* root node */
+    qemu_fdt_setprop_string(ms->fdt, "/", "compatible", "svsm");
+    qemu_fdt_setprop_cell(ms->fdt, "/", "#address-cells", 0x2);
+    qemu_fdt_setprop_cell(ms->fdt, "/", "#size-cells", 0x2);
+    qemu_fdt_setprop_cell(ms->fdt, "/", "device-plane", ms->device_plane);
+
+    dt_setup_sys_bus(ms);
+
+    if (fdt_pack(ms->fdt) < 0) {
+        error_report("x86: Error while packing FDT");
+        return -1;
+    }
+
+    fdt_size = fdt_totalsize(ms->fdt);
+
+    if (debug) {
+        if (g_file_set_contents("qemu.dtb", ms->fdt, fdt_size, NULL)) {
+            fprintf(stderr, "dtb dumped to %s. Exiting\n", "qemu.dtb");
+            exit(0);
+        } else {
+            fprintf(stderr, "error dumping dtb to file %s.", "qemu.dtb");
+        }
+    }
+
+    return fdt_size;
+}
